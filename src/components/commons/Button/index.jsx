ab216edf64280b6956adc36bef8d46b57bdda916
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2021-02-10 18:54:35 -0300

    Fim da aula 08 - Mod01

--- src/components/commons/Button/index.jsx
+++ src/components/commons/Button/index.jsx
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import { TextStyleVariants } from '../../foundation/Text';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 const ButtonGhost = css`
   color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
@@ -18,11 +19,19 @@ export const Button = styled.button`
   padding: 12px 26px;
   font-weight: bold;
   opacity: 1;
-
-  ${TextStyleVariants.smallestException}
-
   transition: opacity ${({ theme }) => theme.transition};
   border-radius: ${({ theme }) => theme.borderRadius};
+
+  ${breakpointsMedia({
+    xs: css`
+      ${TextStyleVariants.smallestException}
+    `,
+    md: css`
+      padding: 12px 43px;
+      ${TextStyleVariants.paragraph1}
+    `,
+  })}
+
   ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
   
   &:hover,
