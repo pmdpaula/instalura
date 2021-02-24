f265adc60e4c721e91e18c86d29f539d4f3887d8
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2021-02-24 16:49:41 -0300

    ESLint e favicon

--- src/theme/utils/breakpointsMedia.js
+++ src/theme/utils/breakpointsMedia.js
@@ -1,14 +1,17 @@
-import { css } from 'styled-components';
-import { breakpoints } from '../index';
+import { css } from 'styled-components'
 
-export const breakpointsMedia = (cssByBreakpoint) => {
-  const breakpointNames = Object.keys(breakpoints);
-  
+import { breakpoints } from '../index'
+
+export const breakpointsMedia = cssByBreakpoint => {
+  const breakpointNames = Object.keys(breakpoints)
+
   return breakpointNames
-    .filter((breakpointName) => Boolean(cssByBreakpoint[breakpointName]))
-    .map((breakpointName) => css`
-      @media only screen and (min-width: ${breakpoints[breakpointName]}px) {
-        ${cssByBreakpoint[breakpointName]}
-      }
-    `);
+    .filter(breakpointName => Boolean(cssByBreakpoint[breakpointName]))
+    .map(
+      breakpointName => css`
+        @media only screen and (min-width: ${breakpoints[breakpointName]}px) {
+          ${cssByBreakpoint[breakpointName]}
+        }
+      `
+    )
 }
